0030be379b207244acbe75332be809d94dd26faf
I admin <dirks.anton96@yandex.ru>
2021-12-27 14:43:41 +0300

    Finish topic one

---
+++ examples from book/from os import getcwd.py
@@ -0,0 +1,19 @@
+#from os import getcwd
+#where_am_I = getcwd()
+
+#import os
+#import sys
+#sys.platform
+#print(sys.platform)
+#print(sys.version)
+
+#print(os.getcwd())
+
+#import datetime
+#print(datetime.date.today())
+
+#print(datetime.date.today().day)
+#print(datetime.date.isoformat(datetime.date.today()))
+
+import time
+print(time.strftime("%I:%M"))
